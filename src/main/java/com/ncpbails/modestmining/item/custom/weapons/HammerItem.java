565d60e39e354fd3acc20b1009f75d176952798a
Nyancatpig <43119979+Nyancatpig@users.noreply.github.com>
2023-05-08 22:03:18 +0930

    Hammer Fix, Missing Loot Tables, and Mangrove Blocks
    Mangrove, yes quite

--- src/main/java/com/ncpbails/modestmining/item/custom/weapons/HammerItem.java
+++ src/main/java/com/ncpbails/modestmining/item/custom/weapons/HammerItem.java
@@ -39,4 +39,19 @@ public class HammerItem extends TieredItem implements Vanishable {
     public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot p_43274_) {
         return p_43274_ == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(p_43274_);
     }
+
+    @Override
+    public boolean hasCraftingRemainingItem() {
+        return true;
+    }
+
+    @Override
+    public ItemStack getCraftingRemainingItem(ItemStack itemStack) {
+        ItemStack retval = new ItemStack(this);
+        retval.setDamageValue(itemStack.getDamageValue() + 1);
+        if (retval.getDamageValue() >= retval.getMaxDamage()) {
+            return ItemStack.EMPTY;
+        }
+        return retval;
+    }
 }
